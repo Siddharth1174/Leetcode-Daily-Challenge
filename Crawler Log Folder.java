0b85f588e29489fc4882a200acdbed42e28e6e68
Siddharth R Sharan <142529706+Siddharth1174@users.noreply.github.com>
2024-07-10 11:57:52 +0530

    Create Crawler Log Folder.java

---
+++ Crawler Log Folder.java
@@ -0,0 +1,20 @@
+//Problem Link:
+https://leetcode.com/problems/crawler-log-folder/
+//Solution:
+class Solution {
+    public int minOperations(String[] logs) {
+         int depth = 0;
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else if (!log.equals("./")) {
+                depth++;
+            }
+        }
+
+        return depth;
+    }
+}
